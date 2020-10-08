950a5e6912dcfe521c790633464ed265a01305b1
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-10-08 23:08:41 +0600

    fix videochat and done it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,11 @@ import store from './redux/store'
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <Router>
         <App />
       </Router>
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
