54be3cdcb90eb00c65885d1fce4ef8271221212f
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-10-08 20:00:23 +0600

    join videochat and chat

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const userRegistration = require('../db/controllers/userControllers/registration');
-const userLogin = require('../db/controllers/userControllers/userLogin');
+const userRegistration = require('../../db/controllers/userControllers/registration');
+const userLogin = require('../../db/controllers/userControllers/userLogin');
 
 const router = express.Router();
 
