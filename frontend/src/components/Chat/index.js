d11e2a6c77c3fa805977fbb3e9303dc3e53e4256
RostislavBartenev <rostislav.bartenev@gmail.com>
2020-10-12 18:03:33 +0600

    done chat

--- frontend/src/components/Chat/index.js
+++ frontend/src/components/Chat/index.js
@@ -1,22 +1,28 @@
 import React, {useEffect} from 'react';
+import {Button} from "@material-ui/core";
 
-function Chat({ users, messages, userName, roomId, onAddMessage, socketRef }) {
+function Chat({ users, messages, userName, roomId, onAddMessage, socketRef, userInfo }) {
     const [messageValue, setMessageValue] = React.useState('');
     const messagesRef = React.useRef(null);
 
     const onSendMessage = () => {
-      socketRef.current.emit('ROOM:NEW_MESSAGE', {
-            userName,
-            roomId,
-            text: messageValue,
+      if (messageValue.trim()) {
+        socketRef.current.emit('ROOM:NEW_MESSAGE', {
+          userName,
+          roomId,
+          text: messageValue,
         });
         onAddMessage({ userName, text: messageValue });
         setMessageValue('');
+      }
+
     };
 
     useEffect(() => {
         messagesRef.current.scrollTo(0, 99999);
     }, [messages]);
+
+
 
     return (
         <div className="chat">
@@ -31,23 +37,30 @@ function Chat({ users, messages, userName, roomId, onAddMessage, socketRef }) {
             <div className="chat-messages">
                 <div ref={messagesRef} className="messages">
                     {messages.map((message, index) => (
+                      message.userName === userInfo ?
+                        <div key={index} style={{textAlign: "right"}} className="message">
+                        <p style={{backgroundColor: 'blue', color: 'white'}}>{message.text}</p>
+                        <div>
+                          <span>{message.userName}</span>
+                        </div>
+                      </div> :
                         <div key={index} className="message">
-                            <p>{message.text}</p>
-                            <div>
-                                <span>{message.userName}</span>
-                            </div>
+                          <p>{message.text}</p>
+                          <div>
+                            <span>{message.userName}</span>
+                          </div>
                         </div>
                     ))}
                 </div>
                 <form>
-          <textarea
-          value={messageValue}
-          onChange={(e) => setMessageValue(e.target.value)}
-          className="form-control"
-          rows="3"/>
-                    <button onClick={onSendMessage} type="button" className="btn btn-primary">
-                        Отправить
-                    </button>
+                  <textarea
+                  value={messageValue}
+                  onChange={(e) => setMessageValue(e.target.value)}
+                  className="form-control"
+                  rows="3"/>
+                  <Button onClick={onSendMessage} type="button" className="btn btn-primary">
+                      Отправить
+                  </Button>
                 </form>
             </div>
         </div>
